7ed8711cc5ae92fadf4da25e53d01d42407d38d6
Bruno Ferro <32205903+BuiuFerro@users.noreply.github.com>
2018-10-31 10:49:21 -0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -5,7 +5,7 @@ class Commands():
     def getCommand(self, msg, msg_id):
         texto = msg["text"]
         if texto[-1::] == "?":
-            self.telegram.sendMessage(msg_id,"Boa!")
+            self.telegram.sendMessage(msg_id,"Agora, coloque as opções.\nUtilize numeros para identifica-las\nQuando terminar, digite: fim")
         elif msg["text"] == "/start":
             self.telegram.sendMessage(msg_id,"Olá para começar me diga qual tipo de evento você pretende ir")
         else:
